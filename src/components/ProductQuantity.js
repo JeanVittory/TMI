4c47ed52438c71e69892778fb0803edf6658e7fd
JeanVittory <jean.carjil@gmail.com>
2022-04-04 16:21:02 -0500

    setting stock

--- src/components/ProductQuantity.js
+++ src/components/ProductQuantity.js
@@ -2,15 +2,25 @@ import add from '../assets/svgs/add.svg'
 import less from '../assets/svgs/less.svg'
 import useQuantity from '../hooks/useQuantity';
 import { useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProductQuantity = ({initialStock, updateStock}) =>{
     const [quantity, currentStock, handlerClickQuantityAdd, handlerQuantityLess] = useQuantity(initialStock)
 
     useEffect(()=>{
         updateStock(currentStock)
+        if(!(currentStock)){
+            toast("You've reached the maximum number of products available!", {
+                bodyClassName: 'font-Mono text-sm'
+            });
+        }
     }, [currentStock])
+
+
     return(
         <div className="flex justify-center item-center">
+            <ToastContainer autoClose={3000} />
             <div className='flex justify-between item-center w-2/5  md:w-3/4  mt-8  p-2  border-y  border-black'>
                 <div className=" border  border-black rounded-full" onClick={handlerQuantityLess}>
                     <img src={less} alt="Less" className='w-6  lg:cursor-pointer'/>
--- src/hooks/useQuantity.js
+++ src/hooks/useQuantity.js
@@ -2,13 +2,13 @@ import {useState} from 'react';
 
 const useQuantity = (initialStock) =>{
     const [quantity, setQuantity] = useState(0);
-    const [currentStock, setCurrentStock] = useState(initialStock)
+    const [currentStock, setCurrentStock] = useState(initialStock);
 
     const handlerClickQuantityAdd = () =>{
         if(currentStock){
             setCurrentStock(prev => prev - 1)
             setQuantity(prev => prev + 1);
-        }
+        };
     };
 
     const handlerQuantityLess = () =>{
