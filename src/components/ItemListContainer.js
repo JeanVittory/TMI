a98f21cb3818a8a0cace1c85fca933af49825dc5
JeanVittory <jean.carjil@gmail.com>
2022-04-06 00:48:08 -0500

    setting product design

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,14 @@
 import Product from "./Product";
+import { SpinnerCircularSplit } from 'spinners-react';
 import {useState, useEffect} from 'react';
 
 const ItemListContainer = () =>{
     const [products, setProducts]= useState([]);
 
     useEffect(() =>{
-        fetchData();
+        setTimeout(() => {
+            fetchData();
+        }, 2000);
     },[]);
     
     const fetchData = async () =>{
@@ -15,9 +18,10 @@ const ItemListContainer = () =>{
     };
     console.log(products)
 
+    
     return(
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {products.map( e => <Product key={e.id} picture= {e.image} name = {e.name} price = {e.price} sizes = {e.size ? e.size:false}/> )}
+        <div className="container mx-auto grid grid-cols-1  justify-center content-center md:grid-cols-2 lg:grid-cols-3  gap-4">
+            {!(products.length)?<div className="container mx-auto w-screen h-screen flex justify-center items-center"><SpinnerCircularSplit size={50} thickness={100} speed={139} color="rgba(172, 57, 57, 1)" secondaryColor="rgba(0, 0, 0, 0.43)" /></div>: products.map( e => <Product key={e.id} picture= {e.image} name = {e.name} price = {e.price} sizes = {e.sizes? e.sizes:false}/> )}
         </div>
     );
 };
--- src/components/ProductPicture.js
+++ src/components/ProductPicture.js
@@ -3,15 +3,15 @@
 const productPicture = ({picture, name, price}) =>{
     return(
         <>
-            <div className='flex flex-col  justify-center items-center'>
-                <img src={picture} alt="Camisa Negra con Logo" className='w-8/12 lg:w-full'/>
+            <div className='flex justify-center items-center w-full md:h-80 md:w-80'>
+                <img src={picture} alt="Camisa Negra con Logo" className=' w-4/5 md:h-full md:w-auto '/>
             </div>
             <div className='flex flex-col  justify-center items-center'>
                 <p className=' w-8/12  lg:w-full  font-Mono  text-sm  pt-3  text-left'>
                     {name}
                 </p>
                 <p className='w-8/12  lg:w-full  font-Mono  text-sm  text-left'>
-                    {price}
+                    ${price.toFixed(2) }
                 </p>
             </div>
         </>
